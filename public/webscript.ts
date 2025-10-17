21657c113bd9a7b056a7ddb4e20b8f83585a2c26
Dillon Matchett <dillon.matchett@gmail.com>
2025-10-17 16:11:08 -0400

    Create webscript.ts

---
+++ public/webscript.ts
@@ -0,0 +1,43 @@
+import {
+  generateTier,
+  formatItem,
+  formatSettlementDetails,
+  formatSurvivor,
+} from "./generator.js";
+
+const form = document.querySelector("form");
+const monster = document.querySelector("#monsterDetails");
+const survivors = document.querySelector("#survivorDetails");
+const gear = document.querySelector("#gearDetails");
+const settlement = document.querySelector("#settlementDetails");
+
+if (form) {
+  form.addEventListener("submit", (event) => {
+    const data = new FormData(form);
+
+    const challenge = generateTier(Number(data.get("tier")));
+
+    if (monster) monster.innerHTML = challenge.monster;
+    if (survivors)
+      survivors.innerHTML = `<div class="character"><p>${formatSurvivor(
+        challenge.survivors[0]
+      )}</p></div> <div class="character"><p>${formatSurvivor(
+        challenge.survivors[1]
+      )}</p></div> <div class="character"><p>${formatSurvivor(
+        challenge.survivors[2]
+      )}</p></div> <div class="character"><p>${formatSurvivor(
+        challenge.survivors[3]
+      )}</p></div>`;
+
+    let gearHTML = "";
+    for (const i of challenge.items) {
+      gearHTML += `<div class="gear"> ${formatItem(i)} </div>`;
+    }
+    if (gear) gear.innerHTML = gearHTML;
+
+    if (settlement && challenge.settlement)
+      settlement.innerHTML = formatSettlementDetails(challenge.settlement);
+
+    event.preventDefault();
+  });
+}
